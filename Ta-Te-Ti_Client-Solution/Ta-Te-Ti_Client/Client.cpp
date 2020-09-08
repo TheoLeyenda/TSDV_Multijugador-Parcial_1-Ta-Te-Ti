039ed4f46b12f881e158e545669354ffaed20f84
theixgamer <theofritaku@gmail.com>
2020-09-08 18:26:58 -0300

    back up

--- Ta-Te-Ti_Client-Solution/Ta-Te-Ti_Client/Client.cpp
+++ Ta-Te-Ti_Client-Solution/Ta-Te-Ti_Client/Client.cpp
@@ -2,6 +2,8 @@
 #include <WS2tcpip.h>
 #include <string>
 #include <string.h>
+#include "../../TaTeTi_Libs/TA-TE-TI/Tablero.h"
+#include "../../Librerias gentille/Libreria.h"
 
 #pragma comment (lib, "ws2_32.lib")
 
@@ -13,15 +15,19 @@
 struct ClientMessage 
 {
 public:
+	char jugada;
 	byte cmd;
 	char data[TAM_MENSAJE];
+	bool inWait;
 };
-
-struct ClientMessage_Move 
+struct ServerRequest
 {
 public:
-	byte pos_move;
+	byte cmd;
+	Tablero *tablero;
+	char messenge[TAM_MENSAJE];
 };
+
 
 int SendMessege(const char* buf, sockaddr_in server, SOCKET out, int isOk)
 {
@@ -30,21 +36,44 @@ int SendMessege(const char* buf, sockaddr_in server, SOCKET out, int isOk)
 	if (sendOk == SOCKET_ERROR)
 	{
 		std::cout << "Hubo un error al enviar" << isOk << std::endl;
-		std::cin.get();
+		cin.get();
+		cin.get();
 		return -1;
 	}
 	return 0;
 }
 
 
-int CheckCommand(char mensaje[TAM_MENSAJE])
+int CheckCommand(ClientMessage &msg)
 {
-	if (mensaje[0] == 'C' && mensaje[1] == ':') 
+	if (msg.data[0] == 'C' && msg.data[1] == ':')
 	{
 		return MENSAJE_CHAT;
 	}
-	else if(mensaje[0] == 'P' && mensaje[1] == ':')
+	else if(msg.data[0] == 'P' && msg.data[1] == ':')
 	{
+		for (int i = 2; i < TAM_MENSAJE; i++)
+		{
+			if (msg.data[i] != ' ')
+			{
+				if (msg.data[i] != '9' && msg.data[i] != '8'
+					&& msg.data[i] != '7' && msg.data[i] != '6'
+					&& msg.data[i] != '5' && msg.data[i] != '4'
+					&& msg.data[i] != '3' && msg.data[i] != '2'
+					&& msg.data[i] != '1')
+				{
+					std::cout << "Ingreso Invalido" << std::endl;
+					cin.get();
+					cin.get();
+					return 0;
+				}
+				else 
+				{
+					msg.jugada = msg.data[i];
+					return MENSAJE_JUGADA;
+				}
+			}
+		}
 		return MENSAJE_JUGADA;
 	}
 	return 0;
@@ -52,12 +81,12 @@ int CheckCommand(char mensaje[TAM_MENSAJE])
 
 int main() 
 {
-	char jugador = 'O';
 	int typeMensaje = 0;
 	std::string ip = "127.0.0.1";
 	sockaddr_in server;
 	WSADATA data;
 	ClientMessage msg;
+	ServerRequest serverRequest;
 	WORD version = MAKEWORD(2, 2);
 	int puerto = 8900;
 	bool ExitChat = false;
@@ -79,45 +108,75 @@ int main()
 	inet_pton(AF_INET, ip.c_str(), &server.sin_addr);
 	SOCKET out = socket(AF_INET, SOCK_DGRAM, 0);
 
-	char buffer[TAM_BUFFER_1KB];
-	memset(buffer, 0, sizeof(buffer));
+
 	int serverSize = sizeof(server);
 
 	//std::cin.get();	
 	while (!ExitChat)
 	{
-
+		//ZONA DE ENVIO DE MENSAJE AL SERVIDOR
 		do 
 		{
 			system("cls");
 			std::cout << "Comandos:" << std::endl;
 			std::cout << "\"C:\" -> indica que lo que se envia es un mensaje al chat del servidor." << std::endl;
 			std::cout << "\"P:\" -> indica que lo que se envia es la jugada del cliente." << std::endl;
-			std::cout << buffer << std::endl;
+			std::cout << "\"E:\" -> indica que desea salir del servidor" << std::endl;
+			//std::cout << buffer << std::endl;
 			std::cout << "> ";
 			std::cin.getline(msg.data, TAM_MENSAJE);
-			typeMensaje = CheckCommand(msg.data);
+			typeMensaje = CheckCommand(msg);
 			msg.cmd = typeMensaje;
+			//std::cout << msg.jugada << std::endl;
 			//std::cout << typeMensaje << std::endl;
 		}
 		while (typeMensaje == 0);
 		
 		SendMessege((char*)&msg, server, out, isOk);
 
+		//------------------------------------------//
+
 		if (msg.data[0] == ExitCharacter)
 		{
 			ExitChat = true;
 		}
+		
+		//ZONA DE RESPUESTA DEL SERVIDOR
+		int bytesIn = recvfrom(out, (char*)&serverRequest, sizeof(serverRequest), 0, (sockaddr*)&server, &serverSize);
+		
+		switch (serverRequest.cmd)
+		{
+		case 1:
+			cout << serverRequest.messenge << endl;
+			cin.get();
+			//cin.get();
+			break;
+		case 2:
+			if (serverRequest.tablero != nullptr) 
+			{
+				//serverRequest.tablero->mostrarTablero(25, 10);
+			}
+			else 
+			{
+				cout << "TABLERO NULO" << endl;
+				cin.get();
+				//cin.get();
+			}
+			break;
+		case 3:
 
-		int bytesIn = recvfrom(out, buffer, sizeof(buffer), 0, (sockaddr*)&server, &serverSize);
+			break;
+		}
 		if (bytesIn == SOCKET_ERROR)
 		{
 			std::cerr << "error al recibir data." << std::endl;
-			std::cin.get();
+			cin.get();
+			cin.get();
 			return -1;
 		}
-		std::cout << buffer << std::endl;
-		std::cin.get();
+		//-----------------------------------------------//
+		//std::cout << buffer << std::endl;
+		//std::cin.get();
 	}
 
 	closesocket(out);
--- Ta-Te-Ti_Server-Solution/Ta-Te-Ti_Server/Server.cpp
+++ Ta-Te-Ti_Server-Solution/Ta-Te-Ti_Server/Server.cpp
@@ -2,28 +2,36 @@
 #include <WS2tcpip.h>
 #include "../../TaTeTi_Libs/TA-TE-TI/Tablero.h"
 #include "../../Librerias gentille/Libreria.h"
+#include <stdlib.h>
+#include <time.h>
 #pragma comment(lib, "ws2_32.lib")
 
 #define TAM_MENSAJE 1500
+#define LIMITE_INFERIOR  1
+#define LIMITE_SUPERIOR  2
 using namespace std;
 
 struct ClientMessage
 {
 public:
+	char jugada;
 	byte cmd;
 	char data[TAM_MENSAJE];
+	bool inWait;
 };
 struct ServerRequest 
 {
 public:
 	byte cmd;
-	char messenge[TAM_MENSAJE];
-	char tablero[TAM_MENSAJE];
+	Tablero *tablero;
+	char messenge[TAM_MENSAJE] = { "Turno del oponente, por favor espere..." };
 };
 
 struct ClientData
 {
 public:
+	char player;
+	int numberPlayer;
 	sockaddr_in client;
 	ClientMessage clientMessage;
 	int clientSize;
@@ -52,8 +60,17 @@ void SettingTypeTable(int &tipoTablero)
 void InitData(int &countClientes, bool &mostrarBytesMensaje)
 {
 	int opcion = 0;
-	cout << "Ingrese la cantidad de clientes: ";
-	cin >> countClientes;
+	do {
+		system("cls");
+		cout << "Ingrese la cantidad de clientes: ";
+		cin >> countClientes;
+		if (countClientes % 2 != 0) 
+		{
+			cout << "cantidad invalida, ingrese una cantidad par..." << endl;
+			cin.get();
+			cin.get();
+		}
+	} while (countClientes % 2 != 0);
 	cout << "Mostrar Informacion personal del cliente ?" << endl;
 	cout << "1: Si." << endl;
 	cout << "2: No." << endl;
@@ -80,6 +97,85 @@ void InitData(int &countClientes, bool &mostrarBytesMensaje)
 		break;
 	}
 }
+bool CheckMove(ClientData &client, int &turno, Tablero* tablero)
+{
+	if (tablero == NULL) return false;
+	cout << "Entre al CheckMove" << endl;
+	bool enableMove = false;
+	if (client.numberPlayer == turno)
+	{
+		cout << "Es mi turno" << endl;
+		enableMove = true;
+		if (turno == 1) 
+		{
+			turno = 2;
+		}
+		else if(turno == 2)
+		{
+			turno = 1;
+		}
+		switch (client.clientMessage.jugada)
+		{
+		case '1':
+			cout << "1" << endl;
+			tablero->colocarJugador(0, 0, client.player);
+			break;
+		case '2':
+			cout << "2" << endl;
+			tablero->colocarJugador(1, 0, client.player);
+			break;
+		case '3':
+			cout << "3" << endl;
+			tablero->colocarJugador(2, 0, client.player);
+			break;
+		case '4':
+			cout << "4" << endl;
+			tablero->colocarJugador(0, 1, client.player);
+			break;
+		case '5':
+			cout << "5" << endl;
+			tablero->colocarJugador(1, 1, client.player);
+			break;
+		case '6':
+			cout << "6" << endl;
+			tablero->colocarJugador(2, 1, client.player);
+			break;
+		case '7':
+			cout << "7" << endl;
+			tablero->colocarJugador(0, 2, client.player);
+			break;
+		case '8':
+			cout << "8" << endl;
+			tablero->colocarJugador(1, 2, client.player);
+			break;
+		case '9':
+			cout << "9" << endl;
+			tablero->colocarJugador(2, 2, client.player);
+			break;
+		}
+	}
+	tablero->mostrarTablero(tablero->GetPosX(), tablero->GetPosY());
+	return enableMove;
+}
+void InitPlayers(ClientData &client_1, ClientData &client_2) 
+{
+	// limite_inferior + rand() % (limite_superior +1 - limite_inferior) ;
+	
+	int randomPlayer = LIMITE_INFERIOR + rand() % (LIMITE_SUPERIOR + 1 - LIMITE_INFERIOR);
+	client_1.numberPlayer = 1;
+	client_2.numberPlayer = 2;
+	switch (randomPlayer)
+	{
+	case 1:
+		client_1.player = 'O';
+		client_2.player = 'X';
+		break;
+	case 2:
+		client_1.player = 'X';
+		client_2.player = 'O';
+		break;
+	}
+}
 void ShowDataClient(ClientData client) 
 {
 	cout << "PESO del mensaje: " << client.bytesInClient << " bytes" << endl;
@@ -88,16 +184,18 @@ void ShowDataClient(ClientData client)
 }
 int main()
 {
-
+	int turno = LIMITE_INFERIOR + rand() % (LIMITE_SUPERIOR + 1 - LIMITE_INFERIOR);
+	srand(time(NULL));
 	int puerto = 8900;
 	//const char* ipHaciaMiMismo = "127.0.0.1";
 	int countClientes = 1;
 	int tipoTablero = 0;
 	int opcion = 0;
 	bool mostrarBytesMensaje = true;
-
+	bool enableMove = true;
 	InitData(countClientes, mostrarBytesMensaje);
 	
+	ServerRequest serverRequest;
 	ClientData* clientes = new ClientData[countClientes];
 
 	char ExitCharacter = '0';
@@ -110,6 +208,7 @@ int main()
 	{
 		cerr << "No pudo iniciar winsock" << endl;
 		cin.get();
+		cin.get();
 		return -1;
 	}
 
@@ -118,6 +217,7 @@ int main()
 	if (listening == INVALID_SOCKET)
 	{
 		cerr << "Invalid socket" << endl;
+		cin.get();
 		cin.get();
 		return -1;
 	}
@@ -133,22 +233,30 @@ int main()
 	{
 		cerr << "No pudo hacer el bind" << endl;
 		cin.get();
+		cin.get();
 		return -1;
 	}
 
 	
-
+	//CREO ESPACIO PARA QUE ENTREN CLIENTES//
 	for (int i = 0; i < countClientes; i++)
 	{
 		clientes[i].clientSize = sizeof(clientes[i].client);
 		clientes[i].idClient = i;
+
 	}
-	
+	//INICIALIZO LA ENTRADA DE LOS CLIENTES//
+	for (int i = 0; i < countClientes - 1; i = i + 2) 
+	{
+		InitPlayers(clientes[i], clientes[i + 1]);
+	}
+
 	SettingTypeTable(tipoTablero);
 
 	Tablero* tablero = new Tablero(tipoTablero, 25, 10);
 	system("cls");
 	tablero->mostrarTablero(tablero->GetPosX(), tablero->GetPosY());
+
 
 	while (inGame) {
 		//funcion bloqueante.
@@ -159,18 +267,42 @@ int main()
 			if (clientes[i].bytesInClient == SOCKET_ERROR)
 			{
 				cerr << "Error al recibir data." << endl;
+				cin.get();
+				cin.get();
 				return -1;
 			}
+			//RESPONDO AL CLIENTE
+			cout << clientes[i].clientMessage.cmd << endl;
 			switch (clientes[i].clientMessage.cmd)
 			{
 			case 1:
 				cout << "MENSAJE_JUGADA" << endl;
+				cout << "Jugada:"<<clientes[i].clientMessage.jugada << endl;
+				cout << "Numero jugador:" << clientes[i].numberPlayer << endl;
+				cout << "turno: " << turno << endl;
+				enableMove = CheckMove(clientes[i], turno, tablero);
+				if (!enableMove) 
+				{
+					serverRequest.cmd = 1;
+					//cout << "INFORME AL JUGADOR QUE NO ES SU TURNO" << endl;
+					
+				}
+				else 
+				{
+					serverRequest.cmd = 2;
+					serverRequest.tablero = tablero;
+					//ENVIAR EL TA TE TI;
+				}
 				break;
 			case 2:
 				cout << "MENSAJE_CHAT" << endl;
+				serverRequest.cmd = 3;
 				break;
 				
 			}
+			sendto(listening, (char*)&serverRequest, sizeof(serverRequest), 0, (sockaddr*)&clientes[i].client, sizeof(clientes[i].client));
+			//----------------------//
+
 			clientes[i].FindIp(AF_INET, &clientes[i].client.sin_addr, clientes[i].ip, 16);
 			clientes[i].port = ntohs(clientes[i].client.sin_port);
 			if (mostrarBytesMensaje)
